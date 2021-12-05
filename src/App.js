b3168715fa373ac8fbd8e2205ae7f6916d705410
Sarah Scardini <sarahscardini@gmail.com>
2021-12-05 18:30:32 -0300

    feat(App): add routes, global style and toast container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import "./styles/reset.css";
+import React from "react";
+import GlobalStyle from "./styles/global";
+import Routes from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <div>
-    </div>
+    <>
+      <ToastContainer theme="colored" />
+      <GlobalStyle />
+      <Routes />
+    </>
   );
 }
 
