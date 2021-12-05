930a109bc93c8ec787017bc7f4ecf02d038c3fdf
Sarah Scardini <sarahscardini@gmail.com>
2021-12-05 18:47:16 -0300

    feat(cartCard): render events card

---
+++ src/components/cartCard/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Button from "../button";
+import { Container } from "./style";
+
+const CartCard = (props) => {
+  const { title, list, remove, buy, removeAll } = props;
+
+  return (
+    <Container>
+      <div className="tableHead">
+        <h2>{title}</h2>
+      </div>
+      {list.toString() ? (
+        <>
+          <ul>
+            {list.map((item, index) => (
+              <li key={index}>
+                <img src={item.image_url} alt={item.name} />
+                <h3>{item.name}</h3>
+                <Button
+                  onClick={() => remove(item)}
+                  text="Remove from List"
+                  name="removeBtn"
+                />
+              </li>
+            ))}
+          </ul>
+          <div className="BtnBox">
+            <Button text="Buy" onClick={buy} name="buy" />
+            <Button text="Remove All" onClick={removeAll} name="removeAll" />
+          </div>
+        </>
+      ) : (
+        <p>Your List is Empty</p>
+      )}
+    </Container>
+  );
+};
+
+export default CartCard;
