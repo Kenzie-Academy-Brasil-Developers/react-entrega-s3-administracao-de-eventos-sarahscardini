fead95fe3970ad02493fdece7e324c204da1a5d5
Sarah Scardini <sarahscardini@gmail.com>
2021-12-06 08:55:51 -0300

    refactor(products): add next and previous page functions instead of render all products at once

--- src/providers/products/index.js
+++ src/providers/products/index.js
@@ -4,23 +4,50 @@ import api from "../../services/api";
 export const ProductsContext = createContext([]);
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+  let token =
+    JSON.parse(localStorage.getItem("@TheGoodStuffProductsCurrPage")) || [];
+  const [products, setProducts] = useState(token);
   const [next, setNext] = useState(1);
+  const [final, setFinal] = useState(false);
 
   useEffect(() => {
     api
       .get(`?page=${next}`)
       .then((response) => {
-        if (response.data) {
-          setProducts((prevList) => [...prevList, ...response.data]);
-          setNext(next + 1);
+        if (response.data.toString()) {
+          setProducts(response.data);
+          localStorage.setItem(
+            "@TheGoodStuffProductsCurrPage",
+            JSON.stringify(response.data)
+          );
+          setFinal(false);
+        } else {
+          setFinal(true);
         }
       })
       .catch((err) => console.log(err));
   }, [next]);
 
+  const nextPage = () => {
+    if (!final) {
+      setNext(next + 1);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const previousPage = () => {
+    if (next > 1) {
+      setNext(next - 1);
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
-    <ProductsContext.Provider value={{ products }}>
+    <ProductsContext.Provider value={{ products, nextPage, previousPage, next, final }}>
       {children}
     </ProductsContext.Provider>
   );
