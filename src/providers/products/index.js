b5509f71b90483063c40cb54b64ba3cb9d878c4e
Sarah Scardini <sarahscardini@gmail.com>
2021-12-06 06:09:02 -0300

    refactor(products): add useEffect to show all the pages

--- src/providers/products/index.js
+++ src/providers/products/index.js
@@ -5,13 +5,19 @@ export const ProductsContext = createContext([]);
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+  const [next, setNext] = useState(1);
 
   useEffect(() => {
     api
-      .get()
-      .then((response) => setProducts([...response.data]))
+      .get(`?page=${next}`)
+      .then((response) => {
+        if (next < 14) {
+          setProducts((prevList) => [...prevList, ...response.data]);
+          setNext(next + 1);
+        }
+      })
       .catch((err) => console.log(err));
-  }, []);
+  }, [next]);
 
   return (
     <ProductsContext.Provider value={{ products }}>
