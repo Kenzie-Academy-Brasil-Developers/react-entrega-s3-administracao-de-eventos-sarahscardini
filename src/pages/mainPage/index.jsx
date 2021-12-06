049c4db56d67b02bde20c1dffef739788bad4139
Sarah Scardini <sarahscardini@gmail.com>
2021-12-06 08:57:48 -0300

    feat(next/previous button): add next and previous button on main page

--- src/pages/mainPage/index.jsx
+++ src/pages/mainPage/index.jsx
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Header from "../../components/header";
 import MainCard from "../../components/mainCard";
 import Details from "../../components/details";
+import Button from "../../components/button";
+import { ProductsContext } from "../../providers/products";
 import { Container } from "./style";
 import { FiArrowUp } from "react-icons/fi";
 
@@ -9,6 +11,8 @@ const MainPage = () => {
   const [openDetails, setOpenDetails] = useState(false);
   const [itemDetails, setItemDetails] = useState();
   const [showArrow, setShowArrow] = useState(false);
+
+  const { nextPage, previousPage, final, next } = useContext(ProductsContext);
 
   const handleDetails = (item) => {
     setOpenDetails(true);
@@ -41,6 +45,18 @@ const MainPage = () => {
     <Container>
       <Header search={true} />
 
+      <div className="buttonsBox">
+        <Button
+          name={next > 1 ? "previousBtn changePage" : "disableBtn changePage"}
+          text="previous page"
+          onClick={previousPage}
+        />
+        <Button
+          name={!final ? "nextBtn changePage" : "disableBtn changePage"}
+          text="next page"
+          onClick={nextPage}
+        />
+      </div>
       {showArrow && (
         <button onClick={scrollToTop} className="toTopBtn">
           <FiArrowUp />
