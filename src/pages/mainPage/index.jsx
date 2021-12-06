0f4ddb7cb65c2cbd0c5a0347cfaf24cb17d2dcb8
Sarah Scardini <sarahscardini@gmail.com>
2021-12-06 06:11:00 -0300

    feat(arrowUpButton): add button to go to page top

--- src/pages/mainPage/index.jsx
+++ src/pages/mainPage/index.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/header";
 import MainCard from "../../components/mainCard";
 import Details from "../../components/details";
+import { Container } from "./style";
+import { FiArrowUp } from "react-icons/fi";
 
 const MainPage = () => {
   const [openDetails, setOpenDetails] = useState(false);
   const [itemDetails, setItemDetails] = useState();
+  const [showArrow, setShowArrow] = useState(false);
 
   const handleDetails = (item) => {
     setOpenDetails(true);
@@ -17,9 +20,33 @@ const MainPage = () => {
     setItemDetails();
   };
 
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowArrow(true);
+      } else {
+        setShowArrow(false);
+      }
+    });
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <>
-      <Header />
+    <Container>
+      <Header search={true} />
+
+      {showArrow && (
+        <button onClick={scrollToTop} className="toTopBtn">
+          <FiArrowUp />
+        </button>
+      )}
+
       <MainCard handleDetails={handleDetails} />
       {openDetails && (
         <Details
@@ -28,7 +55,7 @@ const MainPage = () => {
           item={itemDetails}
         />
       )}
-    </>
+    </Container>
   );
 };
 
